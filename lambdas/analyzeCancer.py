1a4b4ffc68cbd57dea12052874b2bb2841bae13c
Zackaria Ghanbari <zghan2005@gmail.com>
2025-10-25 12:35:03 -0700

    add lambda

---
+++ lambdas/analyzeCancer.py
@@ -0,0 +1,23 @@
+import json
+import base64
+import boto3
+import datetime
+import os
+
+def lambda_handler(event, context):
+    file_content = base64.b64decode(event['body'])
+    bucket_name = 'prismatic-uploads'
+    object_key = f'uploads/cancer_analysis_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.jpg'
+    
+    s3 = boto3.client('s3')
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=object_key,
+        Body=file_content,
+        ContentType='image/jpeg'
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': f'File uploaded to s3://{bucket_name}/{object_key}'
+    }
