cf39713914320891a902a3354f286d72dcb2d26f
501A <art71903design@gmail.com>
2025-10-25 12:12:04 -0700

    Add placeholder for uploaded image

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import UploadInput from "@/components/ui/upload-input";
-import { ArrowRight, Loader } from "lucide-react";
+import ImageInput from "@/components/ui/image-input";
+import { ArrowRight, ImageIcon, Loader } from "lucide-react";
 import { useActionState, useState } from "react";
 
 export default function Home() {
-  const [file, setFile] = useState<File | null>(null);
+  const [imageInput, setImageInput] = useState<File | null>(null);
   const [state, action, pending] = useActionState(
     (prev: unknown, formData: FormData) => {
       const file = formData.get("upload-input") as File;
@@ -14,11 +14,30 @@ export default function Home() {
     undefined
   );
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <form action={action}>
-        <UploadInput file={file} setFile={setFile} />
-        {file && <div>{file.name}</div>}
-        <Button type="submit" className="mt-8 w-full" disabled={pending}>
+    <div className="flex items-center justify-center min-h-screen px-4">
+      <form action={action} className="max-w-xl space-y-4 p-1">
+        {imageInput ? (
+          <div className="bg-zinc-100 border border-zinc-200 p-3 rounded-lg flex items-center gap-4 w-full">
+            <div className="flex-1 flex items-center gap-2 min-w-0">
+              <div className="bg-zinc-300 rounded-full [&_svg]:size-5 p-2">
+                <ImageIcon />
+              </div>
+              <div className="flex-1 min-w-0">
+                <h3 className="truncate">{imageInput.name}</h3>
+              </div>
+            </div>
+            <Button
+              type="button"
+              onClick={() => setImageInput(null)}
+              variant="outline"
+            >
+              Remove
+            </Button>
+          </div>
+        ) : (
+          <ImageInput setImage={setImageInput} />
+        )}
+        <Button type="submit" className="w-full" disabled={pending}>
           {pending ? <Loader className="animate-spin" /> : <ArrowRight />}
           {pending ? "Submitting..." : "Submit"}
         </Button>
--- frontend/components/ui/image-input.tsx
+++ frontend/components/ui/image-input.tsx
@@ -4,15 +4,12 @@ import { Upload } from "lucide-react";
 import React, { useState, useRef } from "react";
 import { Button } from "./button";
 
-export default function UploadInput({
-  file,
-  setFile,
+export default function ImageInput({
+  setImage,
 }: {
-  file: File | null;
-  setFile: React.Dispatch<React.SetStateAction<File | null>>;
+  setImage: React.Dispatch<React.SetStateAction<File | null>>;
 }) {
   const [isDragging, setIsDragging] = useState(false);
-  const [error, setError] = useState<string | null>(null);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   return (
@@ -22,7 +19,7 @@ export default function UploadInput({
         setIsDragging(false);
 
         const droppedFile = e.dataTransfer.files[0];
-        setFile(droppedFile);
+        setImage(droppedFile);
       }}
       onDragOver={(e) => {
         e.preventDefault();
@@ -30,7 +27,7 @@ export default function UploadInput({
       }}
       onDragLeave={() => setIsDragging(false)}
       className={cn(
-        "border border-dashed rounded-xl p-12 text-center transition-all",
+        "border border-dashed rounded-xl p-12 text-center transition-all w-full",
         isDragging
           ? "border-primary bg-primary/5 scale-[1.02]"
           : "border-border hover:border-primary/50 hover:bg-muted/30"
@@ -43,11 +40,9 @@ export default function UploadInput({
 
         <div className="space-y-2">
           <p className="text-lg font-medium">
-            Drop your PDF here or click to browse
+            Drop your image here or click to browse
           </p>
-          <p className="text-sm text-muted-foreground">
-            Maximum file size: 10MB
-          </p>
+          <p className="text-sm text-muted-foreground">...</p>
         </div>
 
         <div>
@@ -57,26 +52,22 @@ export default function UploadInput({
             type="button"
             onClick={() => inputRef.current?.click()}
           >
-            Select PDF File
+            Select Image
           </Button>
 
           <input
-            id="file-upload"
+            id="image-upload"
             name="upload-input"
             ref={inputRef}
-            type="file"
+            type="image"
             accept=".pdf"
             onChange={(e) => {
               const selectedFile = e.target.files?.[0];
-              if (selectedFile) setFile(selectedFile);
+              if (selectedFile) setImage(selectedFile);
             }}
             className="sr-only"
           />
         </div>
-
-        {error && (
-          <p className="text-sm text-destructive font-medium">{error}</p>
-        )}
       </div>
     </div>
   );
